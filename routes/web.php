d49e5292485aec389ce28255c8a87588dab5b1be
unknown <noahdzakki12@gmail.com>
2025-12-03 08:34:10 +0700

    Revisi Routes

--- routes/web.php
+++ routes/web.php
@@ -7,21 +7,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// --- Rute Login Anda ---
-
-// 1. Rute GET untuk menampilkan formulir login Anda (login.blade.php)
-// Pastikan nama view() di sini sesuai dengan nama file Blade Anda (login)
 Route::get('/login', function () {
-    return view('login'); // <-- INI YANG MEMANGGIL login.blade.php Anda
+    return view('login'); 
 })->name('login');
 
-// 2. Rute POST untuk memproses pengiriman data dari formulir login
+
 Route::post('/login', function () {
-    // Logika otentikasi akan ditaruh di sini
+    
     return redirect('/dashboard'); 
 })->name('login.post'); 
 
-// Rute sementara untuk demonstrasi redirect setelah login
+
 Route::get('/dashboard', function () {
     return "Selamat datang di Dashboard!";
 });
